8153846a21b33cf69bde67c857ee24acc6698b4b
Marcel van der Meer <marcelmeer@gmail.com>
2026-08-28 00:20:09 +0200

    https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
 
-        $middleware->append([HttpsRedirect::class]); 
+       $middleware->append([HttpsRedirect::class]); 
     })
     
     ->withExceptions(function (Exceptions $exceptions) {
